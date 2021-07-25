75ab675e59063af63e52707dff647562d92682b9
genki <ggharada39@gmail.com>
2021-07-25 15:37:31 +0900

    logic kansei

--- tutorial/flaskr/views.py
+++ tutorial/flaskr/views.py
@@ -8,17 +8,59 @@ import random
 def show_menus():
     return render_template('show_menus.html')
 
+@app.route('/1000', methods=['GET'])
+def get_1000():
+    money = 1000
+    return(get_menus(money))
 
-@app.route('/get', methods=['GET'])
-def get_menus():
+@app.route('/1500', methods=['GET'])
+def get_1500():
+    money = 1500
+    return(get_menus(money))
 
+@app.route('/2000', methods=['GET'])
+def get_2000():
+    money = 2000
+    return(get_menus(money))
+
+@app.route('/balance', methods=['GET'])
+def balance():
+    menus = []
+    text = "コメダ珈琲バランスガチャを回したよ！" + "\n" + "\n"
+    calories = 0
+    budget = 0
+    dish = ["meal", "drink"]
+
+    #add meal and drink
+    for i in range(2):
+        candidate = db.session.query(Menu).filter(Menu.category == dish[i]).all()
+        food = random.choice(candidate)
+        menus.append(food)
+        text += str(food.name) + "\n"
+        budget += int(food.price)
+        calories += int(food.calories)
+
+    #add dessert
+    candidate = db.session.query(Menu).filter(Menu.type == "dessert", Menu.category == "side").all()
+    food = random.choice(candidate)
+    menus.append(food)
+    text += str(food.name) + "\n"
+    budget += int(food.price)
+    calories += int(food.calories)
+
+    # tweet result
+    text += "\n"
+    text += "計 " + str(budget) + "円 " + str(calories) + "kcal" + "\n" + "\n"
+
+    return render_template('show_menus.html', menus=menus, budget=budget, calories=calories, text=text)
+
+
+def get_menus(money):
     # params
     menus = []
-    text = "サイゼリヤ1000円ガチャを回したよ！" + "\n" + "\n"
-    money = 1000
+    text = "コメダ珈琲" + str(money) + "円ガチャを回したよ！" + "\n" + "\n"
     budget = money
-    calorie = 0
-    salt = 0
+    calories = 0
 
     # select first food
     while not menus:
@@ -29,8 +71,7 @@ def get_menus():
 
     # calc
     budget -= int(menus[0].price)
-    calorie += int(menus[0].calorie)
-    salt += float(menus[0].salt)
+    calories += int(menus[0].calories)
 
     # add text for tweet
     text += str(menus[0].name) + "\n"
@@ -38,8 +79,7 @@ def get_menus():
     while budget > 0:
 
         # avalable food candidate
-        candidate = db.session.query(Menu).filter(
-            Menu.price <= budget).all()
+        candidate = db.session.query(Menu).filter(Menu.price <= budget).all()
 
         # no candidate break
         if not candidate:
@@ -47,6 +87,10 @@ def get_menus():
 
         # select food
         food = random.choice(candidate)
+
+        #被り防止
+        if food in menus:
+            break
 
         # add to list
         menus.append(food)
@@ -56,14 +100,12 @@ def get_menus():
 
         # calc
         budget -= int(food.price)
-        calorie += int(food.calorie)
-        salt += float(food.salt)
+        calories += int(food.calories)
 
     budget = money - budget
 
     # tweet result
     text += "\n"
-    text += "計 " + str(budget) + "円 " + str(calorie) + \
-        "kcal 塩分 " + str(round(salt, 1)) + "g" + "\n" + "\n"
+    text += "計 " + str(budget) + "円 " + str(calories) + "kcal" + "\n" + "\n"
 
-    return render_template('show_menus.html', menus=menus, budget=budget, calorie=calorie, salt=round(salt, 1), text=text)
+    return render_template('show_menus.html', menus=menus, budget=budget, calories=calories, text=text)
