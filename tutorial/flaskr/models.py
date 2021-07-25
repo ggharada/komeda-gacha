04b5052abc2dd0f7700f15b4b7c6e2f889eafb50
genki <ggharada39@gmail.com>
2021-07-25 11:07:58 +0900

    set up

--- tutorial/flaskr/models.py
+++ tutorial/flaskr/models.py
@@ -1,14 +1,14 @@
 from flaskr import db
+from sqlalchemy import Column, Integer, String
 
-class Entry(db.Model):
-    __tablename__ = 'entries'
+class Menu(db.Model):
+    __tablename__ = 'menu'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Text)
-    text = db.Column(db.Text)
-
-    def __repr__(self):
-        return '<Entry id={id} title={title!r}>'.format(
-                id=self.id, title=self.title)
+    name = db.Column(db.String(32))
+    category = db.Column(db.String(32))
+    type = db.Column(db.String(32))
+    price = db.Column(db.Integer)
+    calories = db.Column(db.Integer)
 
 def init():
     db.create_all()
