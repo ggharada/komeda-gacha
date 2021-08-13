ea33ff2ad58543a8341f2f01daed5519e7c847a0
genki <ggharada39@gmail.com>
2021-08-14 00:26:11 +0900

    +おまかせ

--- flaskr/views.py
+++ flaskr/views.py
@@ -87,6 +87,6 @@ def get_menus(money):
 
     # tweet result
     text += "\n"
-    text += "計" + str(budget) + "円 " + str(calories) + "kcal" + "\n" + "#コメダ珈琲ガチャ" + "\n" 
+    text += "計" + str(budget) + "円 " + str(calories) + "kcal" + "\n" + "#コメダ珈琲おまかせガチャ" + "\n" 
 
     return render_template('show_menus.html', menus=menus, budget=budget, calories=calories, text=text)
